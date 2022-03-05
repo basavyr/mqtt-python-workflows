f5105ebb85815d3f8eeee81739f902ca83389aeb
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-03-05 20:40:18 +0200

    Update mqtt_test.py

--- projects/mqtt-simple-test/mqtt_test.py
+++ projects/mqtt-simple-test/mqtt_test.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import platform
+import time
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+ str(rc))
@@ -11,5 +12,7 @@ new_client.on_connect
 print(new_client)
 local_port=1884
 new_client.connect('0.0.0.0',local_port, 60)
-new_client.publish("test/",'hey for v0')
-new_client.publish("test/v2",'hey for v2')
+for i in range(10):
+	new_client.publish("test/",'hey for v0')
+	new_client.publish("test/v2",'hey for v2')
+	time.sleep(1)
